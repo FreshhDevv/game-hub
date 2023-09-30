05d96aaa82c151ecae2f0e25cb3ce75638a4ec4f
FreshDev <billyhans90@gmail.com>
2023-09-30 16:57:17 +0100

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,22 +3,23 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Platform {
-    id: number
-    name: string
-    slug: string
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string
-    parent_platforms: {platform: Platform} []
-  }
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
 
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
