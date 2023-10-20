a665286d66a9c8a21efd6ba30c825c4ea8c33f8d
FreshDev <billyhans90@gmail.com>
2023-10-20 14:52:50 +0100

    Refactor the platform selector component

--- src/App.tsx
+++ src/App.tsx
@@ -42,12 +42,7 @@ function App() {
           <GameHeading />
           <Flex marginBottom={5}>
             <Box marginRight={5}>
-              <PlatformSelector
-                selectedPlatformId={gameQuery.platformId}
-                onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platformId: platform.id })
-                }
-              />
+              <PlatformSelector />
             </Box>
             <SortSelector
               sortOrder={gameQuery.sortOrder}
